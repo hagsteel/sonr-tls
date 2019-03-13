6509da7e41902c8c50305a467baa66f557596764
Jonas Hagstedt <hagstedt@gmail.com>
2019-03-13 21:34:29 +0100

    added error handling and removed unwraps

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{Read, Write};
+use std::io::{self, Read, Write};
 use std::sync::Arc;
 
-use sonr::errors::Result;
+use sonr::errors::Error as SonrError;
 use sonr::net::stream::Stream;
 use sonr::reactor::{Reaction, Reactor};
 
@@ -26,7 +26,7 @@ impl<S> ReactiveTlsAcceptor<S>
 where
     S: Evented + Read + Write,
 {
-    pub fn new(cert_path: &str, cert_pass: &str) -> Result<Self> {
+    pub fn new(cert_path: &str, cert_pass: &str) -> Result<Self, Error> {
         let acceptor = acceptor(cert_path, cert_pass)?;
         Ok(Self {
             acceptor,
@@ -71,19 +71,48 @@ where
                 } else {
                     Reaction::Event(event)
                 }
-                //Reaction::Continue
             }
             Reaction::Continue => Reaction::Continue,
         }
     }
 }
 
-fn acceptor(path: &str, pass: &str) -> Result<Arc<TlsAcceptor>> {
+
+fn acceptor(path: &str, pass: &str) -> Result<Arc<TlsAcceptor>, Error> {
     let mut file = File::open(path)?;
     let mut identity = Vec::new();
     file.read_to_end(&mut identity)?;
-    let identity = Identity::from_pkcs12(&identity, pass).unwrap();
+    let identity = Identity::from_pkcs12(&identity, pass)?;
 
-    let acceptor = TlsAcceptor::new(identity).unwrap();
+    let acceptor = TlsAcceptor::new(identity)?;
     Ok(Arc::new(acceptor))
+}
+
+
+// -----------------------------------------------------------------------------
+// 		- Error -
+// -----------------------------------------------------------------------------
+#[derive(Debug)]
+pub enum Error {
+    Io(io::Error),
+    Sonr(SonrError),
+    Tls(native_tls::Error),
+}
+
+impl From<SonrError> for Error {
+    fn from(err: SonrError) -> Error {
+        Error::Sonr(err)
+    }
+}
+
+impl From<native_tls::Error> for Error {
+    fn from(err: native_tls::Error) -> Error {
+        Error::Tls(err)
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Error {
+        Error::Io(err)
+    }
 }
